329da7a66b88c77702f599d8cd638704bb600262
smartapant <smartapant@gmail.com>
2017-12-12 10:46:55 +0300

    feat(utlis): es5 syntax

--- src/services/utils.js
+++ src/services/utils.js
@@ -1,6 +1,6 @@
 export default {
-  appendKeyframes (name, frames) {
+  appendKeyframes: function (name, frames) {
     var idx = document.styleSheets[0].cssRules.length
-    document.styleSheets[0].insertRule(`@keyframes ${name} { ${frames} }`, idx)
+    document.styleSheets[0].insertRule('@keyframes ' + name + ' { ' + frames + ' }', idx)
   }
 }
