5ca245e31c2ed8ff29762a23520150695cf6ccb0
Nastassia Danilova <nastassia.danilova@epicmax.co>
2019-03-04 17:13:25 +0300

    fix error with reading cssRules property from 'CSSStyleSheet'

--- src/services/utils.js
+++ src/services/utils.js
@@ -1,6 +1,5 @@
 export default {
   appendKeyframes: function (name, frames) {
-    var idx = document.styleSheets[0].cssRules.length
-    document.styleSheets[0].insertRule('@keyframes ' + name + ' { ' + frames + ' }', idx)
+    document.styleSheets[document.styleSheets.length - 1].insertRule('@keyframes ' + name + ' { ' + frames + ' }')
   }
 }
