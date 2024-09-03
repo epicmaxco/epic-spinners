71d6441d03fba9e8c6a75f531e6a7c8a3b85dda2
Yauheni Prakopchyk <yauheni.prakopchyk@epicmax.co>
2024-09-03 17:16:22 +0400

    feat: fix examples

--- packages/docs/vite.config.ts
+++ packages/docs/vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import consoleBuildInfo from "./src/vite-plugins/console-build-info";
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
       vue(),
       consoleBuildInfo(),
+    viteStaticCopy({
+      targets: [
+        { src: path.resolve(__dirname, './static/*'), dest: path.resolve(__dirname, './dist/static') }
+      ],
+    })
   ],
 })
